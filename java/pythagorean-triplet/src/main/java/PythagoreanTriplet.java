d2e1426b5cd19640a1dd31812ce6e94aaef5a644
Pritam <singhrajputpritam224@gmail.com>
2022-01-12 00:17:18 +0530

    Exercism java codes

---
+++ java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -0,0 +1,16 @@
+/*
+
+Since this exercise has a difficulty of > 4 it doesn't come
+with any starter implementation.
+This is so that you get to practice creating classes and methods
+which is an important part of programming in Java.
+
+Please remove this comment when submitting your solution.
+
+*/
+class PythgoreanTriplet{
+    PythgoreanTriplet(int a, int b, int c)
+    {
+
+    }
+}
